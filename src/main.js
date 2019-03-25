ae98fdef5188b1a46b40c1e16d9e3210d26de730
mr_yang_sen@163.com <mr_yang_sen@163.com>
2019-03-25 22:23:31 +0800

    初始化工程01

--- src/main.js
+++ src/main.js
@@ -4,7 +4,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+import Element from 'element-ui';
+
+
 Vue.config.productionTip = false
+
+Vue.use(ElementUI);
+Vue.use(Element, { size: 'small', zIndex: 3000 });
 
 /* eslint-disable no-new */
 new Vue({
